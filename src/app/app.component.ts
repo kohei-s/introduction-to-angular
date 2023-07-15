82b887c939bb044d3a403ee516f4ba88060ccfda
Kohei Suzuki <koheisuzuki@me.com>
2023-07-15 11:43:01 +0200

    modify and completed codelab exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { HousingLocation } from './housing-location';
 
 @Component({
--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -18,13 +18,17 @@ export class HousingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  searchHousingLocations(searchText: string) {
-    if (!searchText) return;
-
-    this.results = this.locationList.filter((location: any) => location.city.toLowerCase().includes(searchText.toLowerCase()));
-  }
-
   selectHousingLocation(location: HousingLocation) {
     this.locationSelectedEvent.emit(location);
   }
+
+  searchHousingLocations(searchText: string) {
+    this.results = this.locationList.filter(
+      (location: HousingLocation) => location.city
+        .toLowerCase()
+        .includes(
+          searchText.toLowerCase()
+        ));
+  }
+
 }
